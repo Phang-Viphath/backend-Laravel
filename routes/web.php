8b97154bf3b0152774b60d5ca9e53cee3ebfddce
viphath12-wq <viphath12@gmail.com>
2026-06-25 15:17:15 +0700

    24

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('/docs', function () {
     return view('scribe.index');
 });
+
+Route::get('/docs.postman', function () {
+    return response()->file(storage_path('app/scribe/collection.json'));
+})->name('scribe.postman');
+
+Route::get('/docs.openapi', function () {
+    return response()->file(storage_path('app/scribe/openapi.yaml'));
+})->name('scribe.openapi');
